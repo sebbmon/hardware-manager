f5b3fb37a56e5edb9b1c28eb317f807d293022fb
Sebastian <sebekmo01@gmail.com>
2026-03-28 00:45:32 +0100

    feat: create serializers and hardware app URL endpoints

---
+++ backend/hardware/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import Hardware, Rental
+
+class HardwareSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hardware
+        fields = '__all__'
+
+class RentalSerializer(serializers.ModelSerializer):
+    hardware_details = HardwareSerializer(source='hardware', read_only=True)
+
+    class Meta:
+        model = Rental
+        read_only_fields = ['user', 'rented_at', 'returned_at', 'is_active']
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'is_staff', 'is_active']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().create(validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
---
+++ backend/hardware/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import HardwareViewSet, RentalViewSet, AdminHardwareViewSet, AdminUserViewSet
+
+router = DefaultRouter()
+router.register(r'hardware', HardwareViewSet, basename='hardware')
+router.register(r'my-rentals', RentalViewSet, basename='rental')
+
+admin_router = DefaultRouter()
+admin_router.register(r'hardware', AdminHardwareViewSet, basename='admin-hardware')
+admin_router.register(r'users', AdminUserViewSet, basename='admin-users')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', include(admin_router.urls)),
+]
